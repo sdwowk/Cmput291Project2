842a92df92b7f8b3f1c83f10094edad72bf03358
Scott Wowk <sdwowk@cs.ualberta.ca>
2015-04-02 14:31:25 -0600

    Project setup for db.jar lol

--- src/cmput291Project2/BinaryTreeTest.java
+++ src/cmput291Project2/BinaryTreeTest.java
@@ -1,17 +1,26 @@
 package cmput291Project2;
+import com.sleepycat.db.*;
 
 public class BinaryTreeTest implements FileTest {
-	
-	public void print() {
-		
-	}
-
-	
-	
-	
+	private static final String BINARY_TABLE = "/tmp/sdwowk_db/Binary_Table";
+	private static final int RECORD_NUM = 10000;
+	/* This is modified from the sample code written by Dr. Li Yuan*/
 	public void createDB() {
-		// TODO Auto-generated method stub
 		
+		// Create the database object.
+	    // There is no environment for this simple example.
+	    DatabaseConfig dbConfig = new DatabaseConfig();
+	    dbConfig.setType(DatabaseType.BTREE);
+	    dbConfig.setAllowCreate(true);
+	    Database my_table = new Database(BINARY_TABLE, null, dbConfig);
+	    System.out.println(BINARY_TABLE + " has been created");
+
+	    /* populate the new database with NO_RECORDS records */
+	    populateTable(my_table,RECORD_NUM);
+	    System.out.println("1000 records inserted into" + BINARY_TABLE);
+
+
+	    
 	}
 
 }
--- src/cmput291Project2/HashMapTest.java
+++ src/cmput291Project2/HashMapTest.java
@@ -2,10 +2,6 @@ package cmput291Project2;
 
 public class HashMapTest implements FileTest {
 
-	public void print() {
-		// TODO Auto-generated method stub
-		
-	}
 
 	public void createDB() {
 		// TODO Auto-generated method stub
