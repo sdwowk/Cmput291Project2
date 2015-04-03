9ec486c90d5de05e5213ddcd0a56b336eb997ab4
sdwowk <sdwowk@ualberta.ca>
2015-04-02 21:33:54 -0600

    Adding skeleton for Index

---
+++ src/cmput291Project2/IndexTest.java
@@ -0,0 +1,17 @@
+package cmput291Project2;
+
+public class IndexTest implements FileTest {
+
+	@Override
+	public void createDB() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
